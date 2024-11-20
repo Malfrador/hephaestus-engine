b49c0d377b01db671a20bb3440b6da16124b3e9d
Malfrador <malfrador@gmail.com>
2024-11-20 20:48:59 +0100

    Change publishing repository

--- buildSrc/src/main/kotlin/hephaestus.publishing-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/hephaestus.publishing-conventions.gradle.kts
@@ -3,47 +3,24 @@ plugins {
     `maven-publish`
 }
 
-val repositoryName: String by project
-val snapshotRepository: String by project
-val releaseRepository: String by project
-
 publishing {
     repositories {
         maven {
-            val snapshot = project.version.toString().endsWith("-SNAPSHOT")
-
-            name = repositoryName
-            url = uri(if (snapshot) { snapshotRepository } else { releaseRepository })
+            name = "erethon"
+            url = uri("https://reposilite.fyreum.de/snapshots/")
             credentials(PasswordCredentials::class)
+            authentication {
+                create<BasicAuthentication>("basic")
+            }
         }
     }
     publications {
         create<MavenPublication>("maven") {
+            groupId = "${project.group}"
+            artifactId = "hephaestus-engine"
+            version = "${project.version}"
+
             from(components["java"])
-            pom {
-                name.set("${project.group}:${project.name}")
-                description.set(project.description)
-                url.set("https://github.com/unnamed/hephaestus-engine")
-                packaging = "jar"
-                licenses {
-                    license {
-                        name.set("MIT License")
-                        url.set("https://opensource.org/license/mit/")
-                    }
-                }
-                developers {
-                    developer {
-                        id.set("yusshu")
-                        name.set("Andre Roldan")
-                        email.set("yusshu@unnamed.team")
-                    }
-                }
-                scm {
-                    connection.set("scm:git:git://github.com/unnamed/hephaestus-engine.git")
-                    developerConnection.set("scm:git:ssh://github.com:unnamed/hephaestus-engine.git")
-                    url.set("https://github.com/unnamed/hephaestus-engine")
-                }
-            }
         }
     }
 }
